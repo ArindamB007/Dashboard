96127cc75547a9818ad901b4bfc616496951522e
ArindamB007 <Crv@98369>
2015-05-07 20:42:49 +0530

    Clean Slate

---
+++ staples-dashboard/project.clj
@@ -0,0 +1,13 @@
+(defproject staples-dashboard "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[ring-server "0.3.1"]
+                 [org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler staples-dashboard.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
---
+++ staples-dashboard/src/staples_dashboard/start_app.clj
@@ -0,0 +1,42 @@
+(ns staples-dashboard.start-app
+(use staples-dashboard.handler
+     ring.server.standalone
+     ring.middleware.file-info
+     ring.middleware.file))
+
+(defn init
+  "This function is called before the server starts"
+  []
+  (println "Staples Dashboard is starting"))
+
+(defn destroy
+  "This function is called before the server shuts down"
+  []
+  (println "Staples Dashboard is shutting down"))
+
+(defonce server (atom nil))
+
+(defn get-handler []
+  (-> #'app
+      (wrap-file "resources")
+      (wrap-file-info)))
+
+(defn start-server
+  "Function allows to start the server in development mode"
+  [& [port]]
+  (let [port (if port (Integer/parseInt port) 8080)]
+    (reset! server
+            (serve (get-handler)
+                   {:port port
+                    :init init
+                    :auto-reload? true
+                    :auto-refresh? true
+                    :destroy destroy
+                    :join true}))
+    (println (str "Staples Dashboard may be view at URL:: http://localhost"))))
+
+(defn stop-server []
+  (.stop @server)
+  (reset! server nil))
+
+(start-server)
